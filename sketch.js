1f4f082400b6ee1b3f89a7f5eb5723b2b1c48ee0
superprog <65724970+superprog@users.noreply.github.com>
2021-09-14 23:24:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ function setup() {
   if(isMobile){
     canW=displayWidth;
     canH=displayHeight;
-    createCanvas(displayWidth+80,displayHeight);
+    createCanvas(displayWidth,displayHeight);
   }
   else{
     canW=windowWidth;
